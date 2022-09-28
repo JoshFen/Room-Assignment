4a77a410c2a5e2461bf19e8cef4739a8fc1b94d0
JesseFentress <jessefentress246@gmail.com>
2022-09-28 13:45:08 -0400

    room/tenantpair

---
+++ src/entities/Room.js
@@ -0,0 +1,38 @@
+class Room {
+
+    constructor(roomNumber, floorNumber, roomSize) {
+        this.roomNumber = roomNumber; 
+        this.floorNumber = floorNumber; 
+        this.roomSize = roomSize;
+        this.tenants = []; // Array to hold students assigned to this room
+        this.isLearningLiving = false;
+    }
+
+    setLearningLiving() {
+        this.isLearningLiving = true;
+    }
+
+    assignPairedTenants(tenants) {
+        if (this.isRoomFull() || this.isRoomAbleToFit()) {
+            return false;
+        }
+        this.tenants.push(tenants.tenant1, tenants.tenant2);
+        return true;
+    }
+
+    assignSingleTenant(tenant) {
+        if (this.isRoomFull()) {
+            return false;
+        }
+        this.tenants.push(tenant);
+        return true;
+    }
+
+    isRoomFull () {
+        return this.tenants.length >= this.roomSize;
+    }
+
+    isRoomAbleToFit() {
+        return this.tenants.length + 2 > this.roomSize;
+    }
+}
---
+++ src/entities/TenantPair.js
@@ -0,0 +1,15 @@
+class TenantPair {
+
+    constructor(firstStudent, secondStudent) {
+        this.firstStudent = firstStudent;
+        this.secondStudent - secondStudent;
+    }
+
+    getFirstStudent() {
+        return this.firstStudent;
+    }
+
+    getSecondStudent() {
+        return this.getSecondStudent;
+    }
+}
