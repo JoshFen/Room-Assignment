0ee569c9354092f8156998110cfe7982d6a05de8
JoshFen <JFentress15@gmail.com>
2022-09-28 12:41:43 -0400

    Student class creation.

---
+++ src/entities/Student.js
@@ -0,0 +1,40 @@
+class Student{
+
+    constructor(sID, fName, lName, preferences){
+        this.sId = sID; // Student ID number.
+        this.fName = fName; // Student first name.
+        this.lName = lName; // Student last name.
+        this.preferences = preferences; // Object of student assignment preferences.
+
+    }
+
+    getSID(){
+        return this.sID;
+    }
+
+    getFName(){
+        return this.fName;
+    }
+
+    getLName(){
+        return this.lName;
+    }
+
+    getPreferences(){
+        return this.preferences;
+    }
+
+    // Getters for individual assignment preferences from preferences object attribute.
+    getRoommatePref(){
+        return this.preferences.roommateID;
+    }
+
+    getLLPref(){
+        return this.preferences.learningLiving;
+    }
+
+    getFloorNumPref(){
+        return this.preferences.floorNum;
+    }
+
+}
