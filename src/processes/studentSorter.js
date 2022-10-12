1be8783429faae16e3d874ffadb8f757e4687474
joshuafentress <JFentress15@gmail.com>
2022-10-12 11:49:16 -0400

    Merge branch 'main' of https://github.com/JoshFen/Room-Assignment

--- src/processes/studentSorter.js
+++ src/processes/studentSorter.js
@@ -1,2 +1,25 @@
 const { readExcel } = require("./processes/fileReader");
 
+function genderSort(){
+ // Accept JSON file and seperate students by gender attribute into a list of male/female students.
+ // Used both to call below functions
+}
+
+function determinePriority(studentArray){
+    // Iterate through array of student objects
+
+    //First check if they're priority is roomate & check if they have a match that is not themselves.
+    
+    // Second check for LLC priority
+
+    //Check for floor priiortity
+}
+
+function roommatePriority(studentArray){
+
+}
+
+function floorPriority(){
+
+}
+
