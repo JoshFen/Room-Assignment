ce60a238643b02ba1339c91187a299ff4d3bd32f
JesseFentress <jessefentress246@gmail.com>
2022-10-30 18:38:28 -0400

    working on dynamic LLCs

--- src/processes/roomBlocker.js
+++ src/processes/roomBlocker.js
@@ -1,3 +1,16 @@
-function LLCBlocking() {
-
+function LLCBlocking(LLCs, floorplan, upperMaleLLC, upperFemaleLLC, lowerMaleLLC, lowerFemaleLLC) {
+    
 }
+
+
+function totalStudentsForEachLLC(uM, uF, lM, lF) {
+    let LLCTotalStudents = {};
+    for (LLC of uM) {
+        LLCTotalStudents[LLC] = uM[LLC].length + uF[LLC].length + lM[LLC].length + lF[LLC].length;
+    }   
+    return LLCTotalStudents; 
+}
+
+module.exports = {
+    LLCBlocking
+  };
