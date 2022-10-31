a57d869829d70db313717ec7948defb0f474ddb9
joshuafentress <JFentress15@gmail.com>
2022-10-31 11:41:24 -0400

    blue print edit

--- src/processes/blueprint.js
+++ src/processes/blueprint.js
@@ -47,9 +47,10 @@ function createBlueprint(floorplanJSON){ // Function to create blueprint from st
         // Check if the object is from the first floor.
         if(data[i]["roomNumber"] < 200){
             ra = isRARoom(data[i]["roomNumber"], 100);
+            roomSize = getRoomSize(data[i]["roomSize"]);
             blueprint["floor"][1]["rooms"][data[i]["roomNumber"]] = {
                 "roomNumber": data[i]["roomNumber"],
-                "roomSize": data[i]["roomSize"],
+                "roomSize": roomSize,
                 "RARoom" : ra,
                 "LLC": false,
                 "sex": ""
@@ -58,9 +59,10 @@ function createBlueprint(floorplanJSON){ // Function to create blueprint from st
         // Check if the object is from the second floor.
         else if(data[i]["roomNumber"] < 300){
             ra = isRARoom(data[i]["roomNumber"], 200);
+            roomSize = getRoomSize(data[i]["roomSize"]);
             blueprint["floor"][2]["rooms"][data[i]["roomNumber"]] = {
                 "roomNumber": data[i]["roomNumber"],
-                "roomSize": data[i]["roomSize"],
+                "roomSize": roomSize,
                 "RARoom" : ra,
                 "LLC": false,
                 "sex": ""
@@ -69,9 +71,10 @@ function createBlueprint(floorplanJSON){ // Function to create blueprint from st
         // Check if the object is from the third floor.
         else if(data[i]["roomNumber"] < 400){
             ra = isRARoom(data[i]["roomNumber"], 300);
+            roomSize = getRoomSize(data[i]["roomSize"]);
             blueprint["floor"][3]["rooms"][data[i]["roomNumber"]] = {
                 "roomNumber": data[i]["roomNumber"],
-                "roomSize": data[i]["roomSize"],
+                "roomSize": roomSize,
                 "RARoom" : ra,
                 "LLC": false,
                 "sex": ""
@@ -80,9 +83,10 @@ function createBlueprint(floorplanJSON){ // Function to create blueprint from st
         // Check if the object is from the fourth floor.
         else if(data[i]["roomNumber"] < 500){
             ra = isRARoom(data[i]["roomNumber"], 400);
+            roomSize = getRoomSize(data[i]["roomSize"]);
             blueprint["floor"][4]["rooms"][data[i]["roomNumber"]] = {
                 "roomNumber": data[i]["roomNumber"],
-                "roomSize": data[i]["roomSize"],
+                "roomSize": roomSize,
                 "RARoom" : ra,
                 "LLC": false,
                 "sex": ""
@@ -91,9 +95,10 @@ function createBlueprint(floorplanJSON){ // Function to create blueprint from st
         // Check if the object is from the fifth floor.
         else if(data[i]["roomNumber"] < 600){
             ra = isRARoom(data[i]["roomNumber"], 500);
+            roomSize = getRoomSize(data[i]["roomSize"]);
             blueprint["floor"][5]["rooms"][data[i]["roomNumber"]] = {
                 "roomNumber": data[i]["roomNumber"],
-                "roomSize": data[i]["roomSize"],
+                "roomSize": roomSize,
                 "RARoom" : ra,
                 "LLC": false,
                 "sex": ""
@@ -146,6 +151,22 @@ function isRARoom(roomNum, floor) {
         }
     }
     return false // This room is not an RA room.
+}
+
+function getRoomSize(jsonRoomSize) {
+    if(jsonRoomSize == "2BD") {
+        return 4;
+    }
+    else if(jsonRoomSize == "3BD") {
+        return 6;
+    }
+    else if(jsonRoomSize == "3BD ADA") {
+        return 4;
+    }
+    else if(jsonRoomSize == "STUDIO") {
+        return 1
+    }
+    return 0;
 }
 
 
